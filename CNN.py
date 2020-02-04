dc2a6ea4a04601366721c39854611355de1613aa
jhammelm <jhammelm@mit.edu>
2020-02-04 11:43:12 -0500

    first commit

--- CNN.py
+++ CNN.py
@@ -5,14 +5,16 @@ from keras.layers import Conv1D, Dense, Reshape, Dropout, LSTM, GlobalMaxPooling
 import tensorflow as tf
 from keras import optimizers
 
+#CNN code for motif convolutional layer
+# taken from https://github.com/uci-cbcl/DanQ
 class CNN():
     def __init__(self,model_layers,seq_shape,out_shape=2,conv_filter_number=100,conv_filter_size=20):
         model=Sequential()
         for i, layer in enumerate(model_layers):
             if layer == 'conv':
                 if i == 0:
-                    JASPAR_motifs = list(np.load('homer_matrix.npy'))
-                    filter_len = max([JASPAR_motifs[k].shape[0] for k in range(len(JASPAR_motifs))])
+                    HOMER_motifs = list(np.load('homer_matrix.npy'))
+                    filter_len = max([HOMER_motifs[k].shape[0] for k in range(len(HOMER_motifs))])
                     conv_layer = Conv1D(input_shape=seq_shape,
                                         filters=len(JASPAR_motifs)*2,
                                         kernel_size=35,
@@ -22,11 +24,11 @@ class CNN():
                     model.add(conv_layer)
                     conv_weights = conv_layer.get_weights()
 
-                    reverse_motifs = [JASPAR_motifs[j][::-1,::-1] for j in range(len(JASPAR_motifs))]
-                    JASPAR_motifs = JASPAR_motifs + reverse_motifs
+                    reverse_motifs = [HOMER_motifs[j][::-1,::-1] for j in range(len(HOMER_motifs))]
+                    HOMER_motifs = HOMER_motifs + reverse_motifs
 
-                    for j in range(len(JASPAR_motifs)):
-                            m = JASPAR_motifs[j][::-1,:]
+                    for j in range(len(HOMER_motifs)):
+                            m = HOMER_motifs[j][::-1,:]
                             w = m.shape[0]
                             conv_weights[0][:,:,j] = 0
                             start = np.random.randint(low=3, high=35-w-3+1)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ensemble-cnn
+# DeepAccess
+
+Code for training, testing, and motif extraction from ensemble of convolutional neural networks for multilabel classification from DNA sequence.
